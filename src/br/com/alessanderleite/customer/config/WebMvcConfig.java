34b68694828fec600c36292589cd65d225db99a1
alessanderleite <alessander.logistica@gmail.com>
2019-07-30 22:24:01 -0300

    create the classes for Config

---
+++ src/br/com/alessanderleite/customer/config/WebMvcConfig.java
@@ -0,0 +1,20 @@
+package br.com.alessanderleite.customer.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan("br.com.alessanderleite")
+public class WebMvcConfig {
+	
+	@Bean(name = "viewResolver")
+	public InternalResourceViewResolver getViewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/META-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		
+		return viewResolver;
+	}
+}
