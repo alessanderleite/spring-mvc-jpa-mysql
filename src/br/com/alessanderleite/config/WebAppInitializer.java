f15e772cdb45c4ffb0f290720e8e698afa5f1f10
alessanderleite <alessander.logistica@gmail.com>
2019-07-31 23:10:40 -0300

    refactoring the packages name

--- src/br/com/alessanderleite/config/WebAppInitializer.java
+++ src/br/com/alessanderleite/config/WebAppInitializer.java
@@ -1,4 +1,4 @@
-package br.com.alessanderleite.customer.config;
+package br.com.alessanderleite.config;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
--- src/br/com/alessanderleite/config/WebMvcConfig.java
+++ src/br/com/alessanderleite/config/WebMvcConfig.java
@@ -1,4 +1,4 @@
-package br.com.alessanderleite.customer.config;
+package br.com.alessanderleite.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
