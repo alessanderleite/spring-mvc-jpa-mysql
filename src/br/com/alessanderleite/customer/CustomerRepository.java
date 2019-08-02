00166ea5eac13e94fb4f9e026c4bd8c25a27ab00
alessanderleite <alessander.logistica@gmail.com>
2019-08-01 22:27:43 -0300

    create the CustomerRepository interface

---
+++ src/br/com/alessanderleite/customer/CustomerRepository.java
@@ -0,0 +1,7 @@
+package br.com.alessanderleite.customer;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long>{
+
+}
